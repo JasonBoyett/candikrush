4f4f693673822ee68141a9c54fc00632a0292b2b
JasonBoyett <jason.boyett@gmail.com>
2023-01-06 04:44:26 -0600

    updating colors #25

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+# I put this python file in so that I can relaod the theme and see how it looks
+# without having to navigate to another directory
+def test_func(num):
+    for i in range(0, num):
+        print(i+1, " test")
+
+
+def main():
+    x = int(input("how many times should I repeat? "))
+    test_func(x)
+
+
+if __name__ == "__main__":
+    main()
